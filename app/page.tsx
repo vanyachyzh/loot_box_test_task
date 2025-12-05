b56298f0ea185110b26ff3ceba3f4e5747ae124e
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2025-12-05 14:46:12 +0200

    Update page1.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import BoxGrid from "./components/BoxGrid";
 
 export default function Home() {
-  0
   return (
     <div className="flex justify-center items-center h-[100vh] px-2 md:px-3">
       <BoxGrid />
